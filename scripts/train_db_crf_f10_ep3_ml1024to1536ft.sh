dc84620854ea34c373ef0a4d665b0f1ddaa0bee9
Your Name <you@example.com>
2022-03-02 17:56:15 +0000

    bug fix

--- scripts/train_db_crf_f10_ep3_ml1024to1536ft.sh
+++ scripts/train_db_crf_f10_ep3_ml1024to1536ft.sh
@@ -10,5 +10,5 @@ python ../src/train.py --fold $EPOCH --model $MODEL --decoder $DECODER --finetun
 --step_scheduler_metric f1 --trans_lr 2e-6 --other_lr 1e-5 --epochs $EP --max_len $MAX_LEN --batch_size $BS --valid_batch_size 4 \
 --input /workspace/feedback-prize-2021 \
 --output /workspace/${MODEL#*/}_${DECODER}_bs${BS}_ml${MAX_LEN}_${DATE} \
---log ../logs/${MODEL#*/}_${DECODER}_${DATE}_ep${EPOCH}.log
---ckpt /workspace/deberta-v3-large_crf_bs4_ml1024_0301/model_${EPOCH}.bin_epoch
+--log ../logs/${MODEL#*/}_${DECODER}_${DATE}_ep${EPOCH}.log \
+--ckpt /workspace/deberta-v3-large_crf_bs4_ml1024_0301/model_${EPOCH}.bin_epoch5
