4f5f327d4f1c6aabc3d0e8cb9a48bca9ce2f8768
Your Name <you@example.com>
2022-03-02 08:26:33 +0000

    fn ft

--- scripts/.ipynb_checkpoints/dp_train_deberta_crf_ep0-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-set -Eeuox
-EPOCH=0
-DATE=0228
-DECODER=crf
-BS=2
-MAX_LEN=1536
-MODEL="microsoft/deberta-v3-large"
-CUDA_HOME=/usr/local/cuda-11
-deepspeed --hostfile=myhostfile ../src/train_dp.py --fold $EPOCH --model $MODEL --decoder $DECODER --step_scheduler_metric f1 --trans_lr 1e-5 --other_lr 1e-3 --epochs 10 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 4 --input /root/feedback-prize-2021 --output /root/autodl-tmp/${MODEL#*/}_${DECODER}_bs${BS}_ml${MAX_LEN}_${DATE} --log ../logs/${MODEL#*/}_${DECODER}_${DATE}_ep${EPOCH}.log --deepspeed --deepspeed_config ../configs/ds_config.json 
--- scripts/.ipynb_checkpoints/train_bigbird_crf_ep3-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-set -Eeuox
-EPOCH=3
-DATE=0227
-DECODER=crf
-BS=4
-MAX_LEN=1536
-MODEL="google/bigbird-roberta-large"
-python ../src/train.py --fold $EPOCH --model $MODEL --decoder $DECODER --step_scheduler_metric f1 --trans_lr 5e-6 --other_lr 1e-3 --epochs 10 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 4 --input /workspace/feedback-prize-2021/ --output /workspace/${MODEL#*/}_${DECODER}_bs${BS}_ml${MAX_LEN}_${DATE} --log ../logs/${MODEL#*/}_${DECODER}_${DATE}_ep${EPOCH}.log
