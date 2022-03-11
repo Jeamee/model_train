de81814758dfa50d2fa4ba4a61e8d71e21c43980
Your Name <you@example.com>
2022-03-11 16:46:52 +0000

    add bb scripts

--- scripts/final_train_bb_crf_f10_ep4.sh
+++ scripts/final_train_bb_crf_f10_ep4.sh
@@ -1,13 +1,13 @@
 set -Eeuox
 EPOCH=4
-DATE=0308
+DATE=0311
 DECODER=crf
 BS=4
 MAX_LEN=1536
 MODEL="google/bigbird-roberta-large"
 
-python ../src/train.py --fold $EPOCH --model $MODEL --decoder $DECODER --freeze 1 --freeze_method soft \
---step_scheduler_metric f1 --trans_lr 1e-5 --other_lr 1e-3 --epochs 10 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 4\
+python ../src/train.py --fold $EPOCH --model $MODEL --decoder $DECODER --freeze 1 --freeze_method soft --gradient_ckpt  --warmup_ratio 0.05 \
+--step_scheduler_metric f1 --trans_lr 1e-5 --other_lr 1e-3 --epochs 10 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 4 \
 --input /workspace/feedback-prize-2021 \
 --output /workspace/${MODEL#*/}_${DECODER}_bs${BS}_ml${MAX_LEN}_${DATE} \
 --log ../logs/${MODEL#*/}_${DECODER}_${DATE}_ep${EPOCH}.log
